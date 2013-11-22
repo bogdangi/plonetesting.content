4cdfca06a1a6ec760ca137359a8b2a80700c91fb
Bogdan Girman <bogdan.girman@gmail.com>
2013-11-22 14:08:47 +0200

    Rename README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='plonetesting.content',
       version=version,
       description="Plone testing content dexterity product",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
